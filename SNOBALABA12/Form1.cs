42640258e7ba062c212b30fe833f4ef8225e32eb
Elnar4233KIT <106557105+PLLNGUS@users.noreply.github.com>
2023-05-17 21:42:37 +0300

    Создал 3 форму, исправил ошибки

--- SNOBALABA12/Form1.cs
+++ SNOBALABA12/Form1.cs
@@ -1,15 +1,39 @@
+using LABA12;
+using Microsoft.VisualBasic.ApplicationServices;
+
 namespace SNOBALABA12
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        private readonly AppDbContext _context;
+        public Form1(AppDbContext context)
         {
             InitializeComponent();
+            _context = context;
         }
 
         private void label5_Click(object sender, EventArgs e)
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var client = new Clients
+            {
+                Name = textBox1.Text,
+                Email = textBox2.Text,
+                Number = textBox3.Text,
+                Password = textBox4.Text
+            };
+
+            _context.Client.Add(client);
+            _context.SaveChanges();
+
+            MessageBox.Show("Вы успешно зарегистрировались!", "Регистрация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Close();
+        }
+
     }
+}
 }
